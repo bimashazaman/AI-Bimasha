443dc644d498610a1ca9366ba93381f4354b8f15
bimashazaman <bimashazaman1234@gmail.com>
2022-08-27 20:04:13 +0600

    Base

--- src/App.jsx
+++ src/App.jsx
@@ -18,22 +18,27 @@ const App = () => (
     <div className="bg-black w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          navbar
+          <Navbar />
         </div>
       </div>
       {/* ending navbar */}
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          States
-          Business
-          Billing
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
